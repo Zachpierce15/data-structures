d6918d5f336768275f3c4d53654aba3f94812fbf
Zachpierce15 <zachary.15pierce@gmail.com>
2019-04-11 17:59:25 -0700

    linkedList completed.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,26 +1,44 @@
 var LinkedList = function() {
+  // debugger;
   var list = {};
   list.head = null;
   list.tail = null;
-
+  
   list.addToTail = function(value) {
+    let newNode = new Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+    }
+    list.tail = newNode;
+    list.head.next = newNode;
   };
 
   list.removeHead = function() {
+    var former = list.head.value;
+    list.head = list.head.next;
+    return former;
   };
 
   list.contains = function(target) {
-  };
-
+    var output = false;
+    var recursion = function(node){
+      if (node.value === target) {
+        return output = true;
+      }
+      else if (node.next !== null) {
+        recursion(node.next);
+      }
+    }
+    recursion(list.head);
+    return output;
+  }
   return list;
 };
 
 var Node = function(value) {
   var node = {};
-
   node.value = value;
   node.next = null;
-
   return node;
 };
 
