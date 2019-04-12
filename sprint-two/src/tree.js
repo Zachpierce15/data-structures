5a750ad7f7b760413ca59e805867a19f757bf118
Zachpierce15 <zachary.15pierce@gmail.com>
2019-04-11 21:52:37 -0700

    Trees completed

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,20 +1,36 @@
+
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
   // your code here
-  newTree.children = null;  // fix me
-
+  //debugger;
+  Object.assign(newTree, treeMethods)
+  newTree.children = [];  // fix me
   return newTree;
 };
 
 var treeMethods = {};
-
+  
 treeMethods.addChild = function(value) {
+  var node = Tree(value);
+  this.children.push(node);
 };
 
 treeMethods.contains = function(target) {
+  debugger;
+  // let boolean = false
+  console.log("this", this.value)
+  if (this.value === target) return true
+  
+  console.log(this.children.length)  
+    for (let i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(target)) {
+        return true;
+      }
+    }
+  return false;
 };
+
 
 
 
