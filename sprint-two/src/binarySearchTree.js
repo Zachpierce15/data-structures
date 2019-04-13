7d67b9e932f3c7d353480c48c6f6e2d493d00a6a
Zachpierce15 <zachary.15pierce@gmail.com>
2019-04-12 21:39:24 -0700

    added functions to the binarySearchTree Function

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,39 @@
-var BinarySearchTree = function(value) {
+var BinarySearchTree = function() {
+  var newTree = {};
+
+  newTree.left = [];
+  newTree.right = [];
+  // your code here
+  //debugger;
+  Object.assign(newTree, treeThings)
+  return newTree;
 };
 
+var treeThings = {};
+treeThings.insert = function(value) {
+  
+}
 
+treeThings.contains = function() {
+  
+}
+
+treeThings.depthFirstLog = function() {
+  
+}
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+// class BinarySearchTree {
+//   constructor {
+//     this.value = value;
+//   }
+//   insert() {};
+  
+//   contains() {};
+  
+//   depthFirstLog() {};
+  
+// }
