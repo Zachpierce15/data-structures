9257d4852855f75d3585fafdc5484c65662e7772
Zachpierce15 <zachary.15pierce@gmail.com>
2019-04-12 21:41:08 -0700

    Finished hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,15 +6,45 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
+
   var index = getIndexBelowMaxForKey(k, this._limit);
-};
+  var hashTable = this._storage;
+
+    if(Array.isArray(hashTable.get(index))) {
+      for (var i =0; i < hashTable.get(index).length; i++){
+        if(hashTable.get(index)[i][0] === k){
+          hashTable.get(index)[i][1] = v;
+        } else {
+          hashTable.get(index).push([k, v]);
+        }
+      }
+    } else {
+      hashTable.set(index, [])
+      hashTable.get(index).push([k, v]); 
+    }
+      console.log("bucketIndex", this._storage.get(index))
+  };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  for(var i = 0; i < bucket.length; i++) {
+    if( bucket[i][0] === k) {
+      return bucket[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  
+  for (i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      bucket.splice(i, 1);
+    }
+  }
+  
 };
 
 
